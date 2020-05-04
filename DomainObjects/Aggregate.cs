ff176bcc6fc331f0355ccdc9fbc0ee2d310216aa
Albert Starreveld <albert.starreveld@rhdhv.com>
2020-05-04 19:25:29 +0200

    Added support for aggregate

---
+++ DomainObjects/Aggregate.cs
@@ -0,0 +1,9 @@
+﻿namespace DomainDrivenDesign.DomainObjects
+{
+    public class Aggregate<T> : Entity<T> where T : Entity<T>
+    {
+        protected Aggregate(Id<T> id) : base(id)
+        {
+        }
+    }
+}
