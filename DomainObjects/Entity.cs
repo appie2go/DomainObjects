092c7b29e16e3798991fff5584189a0213c62888
Albert Starreveld <albert.starreveld@rhdhv.com>
2020-05-04 17:34:51 +0200

    Changing to a namespace thats easier to find

--- DomainObjects/Entity.cs
+++ DomainObjects/Entity.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace DomainObjects
+namespace DomainDrivenDesign.DomainObjects
 {
     public class Entity<T> where T : Entity<T>
     {
--- DomainObjects/Id.cs
+++ DomainObjects/Id.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace DomainObjects
+namespace DomainDrivenDesign.DomainObjects
 {
     public sealed class Id<T> where T : Entity<T>
     {
