e691ecf4397e1005840570726fbab97ca7fbdc43
Albert Starreveld <albert.starreveld@rhdhv.com>
2020-05-04 12:33:07 +0200

    Initial commit

---
+++ DomainObjects/Id.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DomainObjects
+{
+    public sealed class Id<T> where T : Entity<T>
+    {
+        private readonly Guid _id;
+
+        public Id(Guid id)
+        {
+            _id = id;
+
+            if (id == Guid.Empty)
+            {
+                throw new ArgumentException(nameof(id), $"{id} is not a valid identifier.");
+            }
+        }
+
+        public static Id<T> CreateNew()
+        {
+            return new Id<T>(Guid.NewGuid());
+        }
+
+        public override string ToString()
+        {
+            return _id.ToString();
+        }
+
+        public Guid ToGuid()
+        {
+            return _id;
+        }
+
+
+#region Equality
+
+        public static bool operator ==(Id<T> left, Id<T> right)
+        {
+            return left?._id == right?._id;
+        }
+
+        public static bool operator !=(Id<T> left, Id<T> right)
+        {
+            return !(left == right);
+        }
+
+        public bool Equals(Id<T> other)
+        {
+            return this == other;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return this == obj as Id<T>;
+        }
+
+        public override int GetHashCode()
+        {
+            return _id.GetHashCode();
+        }
+
+#endregion
+    }
+}
---
+++ DomainObjects/Value.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DomainObjects
+{
+
+    public class Value<T>
+    {
+        private readonly T _value;
+
+        public Value(T value)
+        {
+            _value = value;
+        }
+
+        public override string ToString()
+        {
+            return _value.ToString();
+        }
+
+
+#region Equality
+
+        public static bool operator ==(Value<T> left, Value<T> right)
+        {
+            if (ReferenceEquals(left, right))
+            {
+                return true;
+            }
+
+            return object.Equals(left._value, right._value);
+        }
+
+        public static bool operator !=(Value<T> left, Value<T> right)
+        {
+            return !(left == right);
+        }
+
+        public bool Equals(Value<T> other)
+        {
+            return this == other;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return this == obj as Value<T>;
+        }
+
+        public override int GetHashCode()
+        {
+            return _value.GetHashCode();
+        }
+
+#endregion
+    }
+
+    public class Value<T1, T2> : Value<Tuple<T1, T2>>
+    {
+        public Value(T1 value1, T2 value2) : base(new Tuple<T1, T2>(value1, value2))
+        {
+        }
+
+        public override string ToString()
+        {
+            return GetType().ToString();
+        }
+    }
+
+    public class Value<T1, T2, T3> : Value<Tuple<T1, T2, T3>>
+    {
+        public Value(T1 value1, T2 value2, T3 value3) : base(new Tuple<T1, T2, T3>(value1, value2, value3))
+        {
+        }
+        public override string ToString()
+        {
+            return GetType().ToString();
+        }
+    }
+
+    public class Value<T1, T2, T3, T4> : Value<Tuple<T1, T2, T3, T4>>
+    {
+        public Value(T1 value1, T2 value2, T3 value3, T4 value4) : base(new Tuple<T1, T2, T3, T4>(value1, value2, value3, value4))
+        {
+        }
+        public override string ToString()
+        {
+            return GetType().ToString();
+        }
+    }
+
+    public class Value<T1, T2, T3, T4, T5> : Value<Tuple<T1, T2, T3, T4, T5>>
+    {
+        public Value(T1 value1, T2 value2, T3 value3, T4 value4, T5 value5) : base(new Tuple<T1, T2, T3, T4, T5>(value1, value2, value3, value4, value5))
+        {
+        }
+        public override string ToString()
+        {
+            return GetType().ToString();
+        }
+    }
+}
